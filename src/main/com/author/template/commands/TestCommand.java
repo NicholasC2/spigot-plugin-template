0ebf95f2023e870e79c2d987b22da53636d0909a
NicholasC2 <137032388+NicholasC2@users.noreply.github.com>
2025-03-31 18:43:29 +1300

    Update TestCommand.java

--- src/main/com/author/template/commands/TestCommand.java
+++ src/main/com/author/template/commands/TestCommand.java
@@ -1,14 +1,10 @@
 package com.author.template.commands;
 
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 
-public class EnrichCommand implements CommandExecutor {
+public class TestCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
