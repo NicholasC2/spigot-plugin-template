bee5c71253104dd0a3c8bd11d217ab0eb4b5b203
NicholasC2 <137032388+NicholasC2@users.noreply.github.com>
2025-04-17 10:48:44 +1200

    Update TestCommand.java

--- src/main/java/com/author/template/commands/TestCommand.java
+++ src/main/java/com/author/template/commands/TestCommand.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandSender;
 public class TestCommand implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         sender.sendMessage("Test Succeeded!");
         return true;
     }
